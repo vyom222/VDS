f2aa1e9500ad6231a013bc4e1b342660d4064ef7
Vyom Chamaria <chamarv@stpaulsschool.org.uk>
2025-01-07 18:57:22 +0000

    battery, motor

--- VDS.py
+++ VDS.py
@@ -12,32 +12,38 @@ G = 9.81
 
 c_rr = 0 # coeff of rolling resistance
 TYRE_PRESSURE_PSI = 30 # psi
-TYRE_PRESSURE = TYRE_PRESSURE_PSI / 14.504
+TYRE_PRESSURE = TYRE_PRESSURE_PSI / 14.504 # convert to bar
 
-C_D = 0.55 # coeff of drag
-C_L = -0.3 # coeff of lift
-C_F = 0.12 # coeff of skin friction
-RHO = 1.125 # fluid density
-CSA = 0.32 # cross-sectional area
+C_D = 0.49 # coeff of drag
+C_L = -0.58 # coeff of lift
+C_S = 0.057 # coeff of skin friction
+RHO = 1.225 # fluid density
+CSA = 0.1535 # cross-sectional area
 WIND_SPEED = 0
 wind_direction = 0 # angle in degrees
 
 V_MAX = 24 # 100% SoC
 V_MIN = 18 # 0% SoC
 SoC = 100 
-Voltage = V_MAX 
+voltage = V_MAX 
 current = 0 
 BAT_R = 0.12 # Battery internal resistance
+H = 20 # Battery Rated Dishcharge time in Hours
+C = 36 # #Battery Rated Capacity at discharge rate in Ah
+K = 1.2 # Estimation for Peukert's constant
+t = 0 # discharge time
 
 rpm = 0
-mot_r = 0 # motor internal resistnace
+mot_r = 1 # motor internal resistnace
+TYRE_DIAMETER = 0.3 # metres
+MOTOR_EFFICIENCY = 0.9
 
 resultant_force = 0
 acceleration = 0
-velocity = 0 # m/s
+velocity = 20 # m/s
 
 def aero():
-    skin_friction = C_F * 0.5 * RHO * velocity ** 2 * CSA # Eq. 21 in research section 3.2.3
+    skin_friction = C_S * 0.5 * RHO * velocity ** 2 * CSA # Eq. 21 in research section 3.2.3
     drag = C_D * 0.5 * RHO * velocity ** 2 * CSA # Eq. 22 in research section 3.2.3
     lift = C_L * 0.5 * RHO * velocity ** 2 * CSA # Eq. 23 in research section 3.2.3
 
@@ -45,6 +51,31 @@ def aero():
     return skin_friction,drag,lift
 
 def rolling_resistance(lift):
-    c_rr = 0.005 + 1 / TYRE_PRESSURE * (0.01 + 0.0095 * (velocity /100)**2) # Eq. 28 in research section 3.2.4
-    F_rr = c_rr * (TOTAL_WEIGHT * G - lift)
+    # Eq. 28 in research section 3.2.4 and conversion to km/h from m/s
+    c_rr = 0.005 + 1 / TYRE_PRESSURE * (0.01 + 0.0095 * (velocity*3.6 /100)**2) 
+
+    F_rr = c_rr * (TOTAL_WEIGHT * G - lift) # Eq. 26 in research sectino 3.2.4
     return F_rr
+
+def Battery(SoC, voltage):
+    current = voltage / ( mot_r + 2 * BAT_R) # Ohm's Law in research section 3.2.2
+    t = H * (C/ current*H) ** K # Peukert's Law Eq. 4 in research section 3.2.1 
+    SoC = SoC - SoC * time_step/t
+    voltage = V_MIN + (V_MAX - V_MIN) * SoC /100 # Eq. 3 in research section 3.2.1
+    return current, SoC, voltage 
+
+def Motor(current, voltage):
+    rpm = velocity * 60 / (math.pi * TYRE_DIAMETER) # Eq. 10 in research section 3.2.2
+    torque = (MOTOR_EFFICIENCY * current * voltage * 60) / (rpm * 2*math.pi) # Eq. 9 in research section 3.2.2
+    return rpm, torque
+
+
+
+
+# skin_friction,drag,lift = aero()
+# print(skin_friction,drag,lift)
+# print(rolling_resistance(lift))
+# for i in range(3600):
+#     current, SoC, voltage = Battery(SoC, voltage)
+#     print(SoC, voltage)
+#     print(Motor(current, voltage))
