d47ae9913d993e52743ded4b1238c6b79ee0d5f7
Vyom Chamaria <chamarv@stpaulsschool.org.uk>
2025-02-22 13:48:37 +0000

    telems data testing

--- powerGraph.py
+++ powerGraph.py
@@ -1,34 +1,43 @@
 import matplotlib.pyplot as plt
 import json
 
-f = open("goodwood2024.txt", "r")
-data = []
+f = open("eastanglia.txt", "r")
 power = []
 times = []
 velocity = []
+current = []
+voltage = []
 time = 0
 
 # Adjust all the data + remove any nulls
 for line in filter(lambda x: len(x.strip()) > 0, f.readlines()):
     try:
         datum = json.loads(line.strip())
-        if datum["p2_I"] != None and datum["24v_V"] != None and datum["gps_spd"] != None and datum['lat'] != None and datum['long'] != None:
-            datum["gps_spd"] *= 1.15078 # Convert from knots to mph
-            if datum['gps_spd'] > 2: # Data less than 10mph isn't very useful
-                data.append(datum)
+        if datum["p2_I"] != None and datum["24v_V"] != None and datum["gps_spd"] != None:
+            datum["gps_spd"] *= 1.15078/2.237 # Convert from knots to metres per second
+            if datum['gps_spd'] > 2: # Data less than 2 metres per second isn't very useful
                 datum['power'] = datum['p2_I']/1000 * datum['24v_V']/1000
                 power.append(datum['power'])
                 velocity.append(datum['gps_spd'])
                 times.append(time)
                 time +=1
+                current.append(datum['p2_I'])
+                voltage.append(datum['24v_V'])
     except Exception as e:
         pass
 
-plt.plot(times,power)
-plt.show()
-plt.plot(times,velocity)
+fig, axs = plt.subplots(2, 2)
+axs[0, 0].plot(times,current)
+axs[0, 0].set_title('Current')
+axs[0, 1].plot(times,velocity, 'tab:orange')
+axs[0, 1].set_title('Velocity')
+axs[1, 0].plot(times,voltage, 'tab:green')
+axs[1, 0].set_title('Voltage')
+axs[1, 1].plot(times,power, 'tab:red')
+axs[1, 1].set_title('Power')
+fig.tight_layout()
+
 plt.show()
 
-
-for p in power:
-    print(p)
+# for p in power:
+#     print(p)
