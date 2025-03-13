c5a01a076431cb3cd97bb6e2075c964bc2611eef
Vyom Chamaria <chamarv@stpaulsschool.org.uk>
2025-03-13 21:47:55 +0000

    telems analysis

--- powerGraph.py
+++ powerGraph.py
@@ -1,12 +1,13 @@
 import matplotlib.pyplot as plt
 import json
 
-f = open("eastanglia.txt", "r")
+f = open("goodwood2025_fp.txt", "r")
 power = []
 times = []
 velocity = []
 current = []
 voltage = []
+alt = []
 time = 0
 
 # Adjust all the data + remove any nulls
@@ -17,9 +18,11 @@ for line in filter(lambda x: len(x.strip()) > 0, f.readlines()):
             datum["gps_spd"] *= 1.15078/2.237 # Convert from knots to metres per second
             if datum['gps_spd'] > 2: # Data less than 2 metres per second isn't very useful
                 datum['power'] = datum['p2_I']/1000 * datum['24v_V']/1000
+
                 power.append(datum['power'])
                 velocity.append(datum['gps_spd'])
                 times.append(time)
+                alt.append(datum['alt'])
                 time +=1
                 current.append(datum['p2_I'])
                 voltage.append(datum['24v_V'])
@@ -38,6 +41,7 @@ axs[1, 1].set_title('Power')
 fig.tight_layout()
 
 plt.show()
-
+plt.plot(times, alt)
+plt.show()
 # for p in power:
 #     print(p)
